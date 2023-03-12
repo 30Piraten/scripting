ecddb8324f716486c340775171a28b7c9cfc1276
30Piraten <kioyarautenberg@gmail.com>
2023-03-12 08:28:58 +0200

    Add comments to helloWorld

--- helloWorld.sh
+++ helloWorld.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# for referencing a variable we use the $ sign.
+# we use this to avoid name collisions. 
+# so ls -> on the terminal list storage; while $ls prints the value
+# stored in the  variable name.
+# also note that variables by defailt don't persist --> when used directly on the terminal
+# and not inside a bash script. 
+
+echo "Hello world"
+
+# creating variables
+# double quotes are used to get the values in the variable
+
+NAME="Kioya Rautenberg"
+CLASS="Piraten"
+
+echo "Mein Name ist $NAME und ich bin Mitglied von dreibig $CLASS."
 
 
-echo "Hello World"
